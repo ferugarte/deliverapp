d0acb5e27806d7d3a8520b03c7a2ae116dc29fe9
ferugarte <ferugarte@gmail.com>
2023-12-26 21:15:03 -0300

    netlify

--- .history/src/index_20231226211142.js
+++ .history/src/index_20231226211142.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './firebase/config'
+import app from './firebase/config'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
